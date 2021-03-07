519800ecff44e3216cfd4cf6db1d5aaf0dc0feca
Weslley Quadros <wquadros@uolinc.com>
2021-03-07 15:14:01 -0300

    Initial Commit

---
+++ CircleProgressBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CircleProgressBar
+//
+//  Created by Weslley Tavares de Aguiar e Quadros on 07/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
